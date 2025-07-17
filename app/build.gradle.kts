02e19156f1f5c5a6ae865849a3056a7b1393cbf0
Egor <egorkrasnov123456789@gmail.com>
2025-07-17 13:07:19 +0700

    добавление главных room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    id("com.google.devtools.ksp") // ✅ добавь этот
 }
 
 android {
@@ -40,10 +42,20 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.navigation3.ui)
+
+    implementation("androidx.room:room-runtime:2.7.2")
+    implementation("androidx.room:room-ktx:2.7.2")
+    ksp("androidx.room:room-compiler:2.7.2")
+
     implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
-    implementation(libs.androidx.material3.adaptive.navigation3)
+    implementation(libs.androidx.adaptive.navigation3)
+    implementation (libs.androidx.adaptive)
+    implementation (libs.androidx.adaptive.layout)
+    implementation(libs.androidx.material3.windowsizeclass)
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.landscapist.coil)
     implementation(libs.ktor.client.core)
